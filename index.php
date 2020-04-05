05c2dcf25b0db9107c2d1ddd47e3303a1bc40144
Patrick Yoon <45907814+yoonpatrick3@users.noreply.github.com>
2020-04-04 20:03:04 -0500

    heroku page

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    Test
+</head>
+
 <?php
     //$url = 'C:/Users/12244/yoonp/independentCS/kpop/kpop_data.json'
     //$data = file_get_contents($url); // put the contents of the file into a variable
@@ -6,3 +11,5 @@
     $testvar = 'test'
     echo $testvar;
 ?>
+
+</html>
