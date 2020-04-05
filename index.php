a74311def415bcdbbc6be4906dc29085dbdac499
Patrick Yoon <45907814+yoonpatrick3@users.noreply.github.com>
2020-04-04 19:37:03 -0500

    heroku page

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    //$url = 'C:/Users/12244/yoonp/independentCS/kpop/kpop_data.json'
+    //$data = file_get_contents($url); // put the contents of the file into a variable
+    //$characters = json_decode($data); // decode the JSON feed
+
+    $testvar = 'test'
+    echo $testvar;
+?>
